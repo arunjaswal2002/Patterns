ad9bd45bb233c84d8c177326867c51cf1224b392
Arun Jaswal <75573505+arunjaswal2002@users.noreply.github.com>
2022-03-10 22:17:07 +0530

    Create Pattern2.cpp

---
+++ Pattern2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+    int row, col;
+    cin >> row;
+
+  
+    for (int i = 1; i <= row; i++) {
+        for (int j = 1; j <= row; j++) {
+            if (j - i >= 0 && j - i <= row) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// *****
+//  ****
+//   ***
+//    **
+//     *
